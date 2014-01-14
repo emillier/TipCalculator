40b607dd1c56a22848843d2423cb8828ef1d13f8
Emillie Ruston <emillie.ruston@kaldorgroup.com>
2014-01-14 13:59:22 +0000

    Round tip and total to two decimal places

--- tipcalculator.js
+++ tipcalculator.js
@@ -2,26 +2,6 @@
 
   Feedback:
 
-  Use single var statements in each scope (our styleguide) DONE
-
-  Indentation should use 2 spaces (our styleguide) DONE
-
-  Inconsistent indentation. If you start doing this early on it will become natural quickly. 
-  Improves code readability. DONE
-
-  Consider writing functions that perform your calculations. This improves other peoples 
-  understanding of what the code does as the function name will tell them.
-
-  Consider adding £ signs into your tip and total DONE
-
-  Add a trailing 0 to the tip / total values if required.
-   --> function that converts numbers to currency
- +
-  Round tip and total values to 2 decimal places
-
-  Consider only displaying the Tip and Total spaces after the calculation
-    class e.g. is-hidden and then toggle it on when the form is submitted DONE
-
   Make it so you can hit return to submit? Key up event
 
 */
@@ -46,24 +26,8 @@
           return ( billTotalValue / 100 ) * tipPercentageValue;
       },
 
-      formatCurrency = function( value ) {
-        var valueAsString = '' + value,
-          valueParts = valueAsString.split('.'),
-          pence = valueParts[1],
-          penceLength = pence.length;
-        
-        if ( penceLength === 1 ) {
-
-          valueParts[1] = valueParts[1] + '0';
-
-// Got as far as splitting the value, need to join value parts together again then return from function 
-
-
-        }
-
-        console.log(valueParts);
-
-
+      numberToTwoDecimalPlaces = function( value ) {
+        return value.toFixed(2);
       };
 
     submitButton.addEventListener( 'click', function() {
@@ -72,27 +36,17 @@
 
         tipPercentageValue = tipPercentage.value;
 
-        console.log( billTotalValue, tipPercentageValue )
-
-
       if ( billTotalValue === '' || tipPercentageValue === '' ) {
         return false;
       }
-
-      // if billtotalvalue is an empty string
-      // or
-      // tippercentagevalue is an empty string
-      // return false
 
       var suggestedTipValue = getSuggestedTipValue( billTotalValue, tipPercentageValue ),
 
         suggestedTotalValue =  ( billTotalValue * 1 ) + suggestedTipValue;
 
-      formatCurrency( suggestedTipValue );
+      tip.innerHTML = '&pound;' + numberToTwoDecimalPlaces( suggestedTipValue );
 
-      tip.innerHTML = '&pound;' + suggestedTipValue;
-
-      total.innerHTML = '&pound;' + suggestedTotalValue;
+      total.innerHTML = '&pound;' + numberToTwoDecimalPlaces( suggestedTotalValue );
 
       tip.classList.remove(IS_HIDDEN_CLASS);
 
